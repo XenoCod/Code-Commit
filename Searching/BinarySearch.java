7323a09a7b07e4ac2639c6b4e845b88d461a5e32
ADITYA <adi@gmail.com>
2021-06-23 23:58:55 +0530

    Binary Search

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,32 @@
+package Codes.Searching;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+    //Iterative Approach
+    static int binarySearch(int arr[], int n, int x){
+        int low=0, high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==x)
+                return mid;
+            else if(arr[mid]>x)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter size");
+        int n=sc.nextInt();
+        System.out.println("Enter element to be searched");
+        int x=sc.nextInt();
+        int arr[]= new int[n];
+        System.out.println("Enter elements");
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        System.out.println(binarySearch(arr,n,x));
+    }
+}
