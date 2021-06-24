fd3f8282e0ba55879e9db34bb7aa64ad21b764ef
Aditya Singh <adityakrsingh1999@gmail.com>
2021-06-24 13:17:14 +0530

    Sqaure root

---
+++ Searching/SquareRootOfN.java
@@ -0,0 +1,17 @@
+package Codes.Searching;
+
+import java.util.Scanner;
+
+public class SquareRootOfN {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter size");
+        int n=sc.nextInt();
+        System.out.println("Enter element to be searched");
+        int x=sc.nextInt();
+        int arr[]= new int[n];
+        System.out.println("Enter elements");
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+    }
+}
