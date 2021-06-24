d2a8cecab4dd5fc5b03b55b5a73ba72845283946
Aditya Singh <adityakrsingh1999@gmail.com>
2021-06-24 14:53:35 +0530

    Square root

--- Searching/SquareRootOfN.java
+++ Searching/SquareRootOfN.java
@@ -3,15 +3,33 @@ package Codes.Searching;
 import java.util.Scanner;
 
 public class SquareRootOfN {
+    static int squareRoot(int n){
+        //for a value of n we could divide it from 1 to n such that 1...n/4...n/2...n
+//        and then we apply binary seacrh cuz it is in sorted form
+        int low=1;
+        int high=n;
+        int res=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int sqr=mid*mid;
+            if(n==sqr)
+                return mid;
+            else if(sqr>n)
+                high=mid-1;
+            else {
+                low = mid + 1;
+                res = mid;
+            }
+
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter size");
         int n=sc.nextInt();
-        System.out.println("Enter element to be searched");
-        int x=sc.nextInt();
-        int arr[]= new int[n];
-        System.out.println("Enter elements");
-        for(int i=0;i<n;i++)
-            arr[i]=sc.nextInt();
+        System.out.println(squareRoot(n));
+
     }
 }
