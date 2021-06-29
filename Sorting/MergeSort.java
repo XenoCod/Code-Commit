24928401777b9752ea2ee43e70ec5b19e8fb5837
Aditya Singh <adityakrsingh1999@gmail.com>
2021-06-29 21:27:55 +0530

    Merge Sort

---
+++ Sorting/MergeSort.java
@@ -0,0 +1,17 @@
+package Codes.Sorting;
+
+import java.util.Scanner;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter size");
+        int n=sc.nextInt();
+        System.out.println("Enter the no of students");
+        int k=sc.nextInt();
+        int arr[]= new int[n];
+        System.out.println("Enter elements");
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+    }
+}
