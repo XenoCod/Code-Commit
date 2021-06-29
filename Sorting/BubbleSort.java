7e41f9255ad265ed9a606635c4833038f8abaebc
Aditya Singh <adityakrsingh1999@gmail.com>
2021-06-30 02:45:46 +0530

    Bubble Sort Optimized

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -3,6 +3,22 @@ package Codes.Sorting;
 import java.util.Scanner;
 
 public class BubbleSort {
+
+    static void bubbleSort(int arr[], int n){
+        for(int i=0;i<n-1;i++){
+            boolean swapped=false;
+            for(int j=0;j<n-i-1;j++)
+                if(arr[j]>arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+
+                if(!swapped)
+                    break;
+        }
+    }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter size");
@@ -11,5 +27,8 @@ public class BubbleSort {
         System.out.println("Enter elements");
         for(int i=0;i<n;i++)
             arr[i]=sc.nextInt();
+        bubbleSort(arr, n);
+        for(int i=0;i<n;i++)
+            System.out.println(arr[i]);
     }
 }
