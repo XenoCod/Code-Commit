497f88f870a3de92931a84f2507e84e5a2552c8c
Aditya Singh <adityakrsingh1999@gmail.com>
2021-06-30 16:59:25 +0530

    Sort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -3,15 +3,31 @@ package Codes.Sorting;
 import java.util.Scanner;
 
 public class SelectionSort {
+    static void selectionSort(int arr[], int n){
+        for(int i=0;i<n-1;i++){
+            int min_ind=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min_ind]){
+                    min_ind=j;
+                }
+            }
+            int temp=arr[min_ind];
+            arr[min_ind]=arr[i];
+            arr[i]=temp;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter size");
         int n=sc.nextInt();
-        System.out.println("Enter the no of students");
-        int k=sc.nextInt();
         int arr[]= new int[n];
         System.out.println("Enter elements");
         for(int i=0;i<n;i++)
             arr[i]=sc.nextInt();
+        selectionSort(arr, n);
+        for(int i=0;i<n;i++)
+            System.out.println(arr[i]);
+
     }
 }
