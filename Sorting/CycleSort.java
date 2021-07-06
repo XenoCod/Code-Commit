8a8d5676c533ae5485a1a6e181b9c87459ed7452
Aditya Singh <adityakrsingh1999@gmail.com>
2021-07-06 11:51:20 +0530

    Sorting

--- Sorting/CycleSort.java
+++ Sorting/CycleSort.java
@@ -6,31 +6,33 @@ public class CycleSort {
 
 //This sorting algo mostly used to sorting the array with minnmum swaps
     static void cycleSort(int arr[], int n){
-        for(int start=0;start<n-1;start++){
-            int item=arr[start];
-            int pos=start;
-            for(int i=start+1;i<n;i++)
+        for(int cs=0;cs<n-1;cs++){
+            int item=arr[cs];
+            int pos=cs;
+            for(int i=cs+1;i<n;i++)
                 if(arr[i]<item)
                     pos++;
-            int temp=arr[pos];
-            arr[pos]=item;
-            item=temp;
-            while(start!=pos){
-                pos=start;
-                for(int i=start+1;i<n;i++)
+            //swap(item,arr[pos])
+
+            int temp=item;
+            item=arr[pos];
+            arr[pos]=temp;
+            
+            while(pos!=cs){
+                pos=cs;
+                for(int i=cs+1;i<n;i++)
                     if(arr[i]<item)
                         pos++;
 
-                    temp=arr[pos];
-                    arr[pos]=item;
-                    item=temp;
+                //swap(item,arr[pos])
+                temp=item;
+                item=arr[pos];
+                arr[pos]=temp;
             }
-
-
-
         }
-
     }
+
+
 
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
@@ -38,8 +40,9 @@ public class CycleSort {
         int n=sc.nextInt();
         int arr[]= new int[n];
         System.out.println("Enter the elememts");
-        for(int i=0;i<n;i++)
-            arr[i]=sc.nextInt();
+        for(int i=0;i<n;i++) {
+            arr[i] = sc.nextInt();
+        }
         cycleSort(arr,n);
         for(int i=0;i<n;i++)
             System.out.println(arr[i])  ;
