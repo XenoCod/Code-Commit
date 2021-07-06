9a473c17adbc3d4955e6086bb6e328dcc83d1f1a
Aditya Singh <adityakrsingh1999@gmail.com>
2021-07-06 20:13:05 +0530

    Matrix

--- Matrix/SimpleJagged2DArray.java
+++ Matrix/SimpleJagged2DArray.java
@@ -7,7 +7,7 @@ public class SimpleJagged2DArray {
         for(int i=0;i<arr.length;i++){
             arr[i]=new int [i+1];
             for(int j=0;j<arr[i].length;j++){
-                arr[i][j]=i;
+                arr[i][j]=i+1;
                 System.out.print(arr[i][j]+" ");
             }
 
