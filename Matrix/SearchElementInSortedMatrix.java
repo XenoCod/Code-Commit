622c61d2192973805e20baf7ddcc29a6b6d4c9bc
Aditya Singh <adityakrsingh1999@gmail.com>
2021-07-08 23:07:09 +0530

    Matrix

---
+++ Matrix/SearchElementInSortedMatrix.java
@@ -0,0 +1,31 @@
+package Codes.Matrix;
+
+public class SearchElementInSortedMatrix {
+    static void printLocation(int arr[][], int k){
+        int row=4,column=5;
+        int i=0, j=column-1;
+        while(i<row && j>=0){
+            if(arr[i][j]==k) {
+                System.out.println("Element at index at row " + (i+1) + " & coloum " + (j+1));
+                return;
+            }
+
+            else if (arr[i][j]>k)
+                j--;
+            else
+                i++;
+
+        }
+        System.out.println("Not found");
+
+
+    }
+    public static void main(String[] args) {
+        int arr[][] = {{1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14 , 15},
+                {16, 17, 18, 19, 20}};
+        int k=11;//the elment to be searched
+        printLocation(arr, k);
+    }
+}
