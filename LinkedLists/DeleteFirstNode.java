4768500b6e1a6894f272296a15f08ae421f80c86
Aditya Singh <adityakrsingh1999@gmail.com>
2021-07-15 13:05:22 +0530

    Linked List

--- LinkedLists/DeleteFirstNode.java
+++ LinkedLists/DeleteFirstNode.java
@@ -5,11 +5,19 @@ import java.awt.*;
 import static Codes.LinkedLists.TraversalOfLinkedLists.printList;
 
 public class DeleteFirstNode {
+    public static Node deleteFirstNode(Node head){
+        if(head==null)
+            return null;
+        else
+            return head.next;
+    }
     public static void main(String[] args) {
         Node head= new Node(10);
         Node temp1= new Node(20);
         Node temp2= new Node(30);
-        deleteFirstNode(head);
+        head.next=temp1;
+        temp1.next=temp2;
+        head=deleteFirstNode(head);
         printList(head);
     }
 }
