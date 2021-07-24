2251131ff0d85241129991c9caf8a42249411074
Aditya Singh <adityakrsingh1999@gmail.com>
2021-07-24 16:23:08 +0530

    Heaps

--- Heap/KthClosestElements.java
+++ Heap/KthClosestElements.java
@@ -1,73 +1,146 @@
-package Codes.Heap;
-
-import java.lang.annotation.Inherited;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
-class Pair{
-    int key;
-    int value;
-    Pair(int k, int v){
-        this.key=k;
-        this.value=v;
-    }
-    int getKey(){
-        return key;
-    }
-    void setKey(int k){
-        this.key=k;
-    }
-    int getValue(){
-        return value;
-    }
-    void setValue(int v){
-        this.value=v;
-    }
-
-}
-
-
-
-public class KthClosestElements {
-    static void kthClosest(int arr[], int x, int k){
-        PriorityQueue<Pair> pq = new PriorityQueue<>(
-                new Comparator<Pair>()
-                {
-                    @Override
-                    public int compare(Pair p1, Pair p2)
-                    {
-                        return p2.getValue().compareTo(
-                              p1.getValue());
-                    }
-                });
-
-        for(int i = 0; i < k; i++)
-        {
-            pq.offer(new Pair(arr[i],
-                    Math.abs(arr[i] - x)));
-        }
-
-        for(int i = k; i < arr.length; i++)
-        {
-            int diff = Math.abs(arr[i] - x);
-
-            if(pq.peek().getValue()>diff) {
-                pq.poll();
-                pq.offer(new Pair(arr[i], diff));
-            }
-        }
-
-        while(!pq.isEmpty())
-        {
-            System.out.print(pq.poll().getKey() + " ");
-        }
-
-
-    }
-    public static void main(String[] args) {
-        int arr[]={40,41,2,4,12,5,6,113,77};
-        int num=43;
-        int k=3;
-       kthClosest(arr,num ,k );
-    }
-}
+//package Codes.Heap;
+//
+//import java.lang.annotation.Inherited;
+//import java.util.Comparator;
+//import java.util.PriorityQueue;
+//
+//class Pair{
+//    int key;
+//    int value;
+//    Pair(int k, int v){
+//        this.key=k;
+//        this.value=v;
+//    }
+//    int getKey(){
+//        return key;
+//    }
+//    void setKey(int k){
+//        this.key=k;
+//    }
+//    int getValue(){
+//        return value;
+//    }
+//    void setValue(int v){
+//        this.value=v;
+//    }
+//
+//}
+//
+//
+//
+//public class KthClosestElements {
+//    static void kthClosest(int arr[], int x, int k){
+//        PriorityQueue<Pair> pq = new PriorityQueue<>(
+//                new Comparator<Pair>()
+//                {
+////                    @Override
+////                    public int compare(Pair p1, Pair p2)
+////                    {
+////                        return p2.getValue().compareTo(
+////                              p1.getValue());
+////                    }
+//                });
+//
+//        for(int i = 0; i < k; i++)
+//        {
+//            pq.offer(new Pair(arr[i],
+//                    Math.abs(arr[i] - x)));
+//        }
+//
+//        for(int i = k; i < arr.length; i++)
+//        {
+//            int diff = Math.abs(arr[i] - x);
+//
+//            if(pq.peek().getValue()>diff) {
+//                pq.poll();
+//                pq.offer(new Pair(arr[i], diff));
+//            }
+//        }
+//
+//        while(!pq.isEmpty())
+//        {
+//            System.out.print(pq.poll().getKey() + " ");
+//        }
+//
+//
+//    }
+//    public static void main(String[] args) {
+//        int arr[]={40,41,2,4,12,5,6,113,77};
+//        int num=43;
+//        int k=3;
+//       kthClosest(arr,num ,k );
+//    }
+//}
+//package Codes.Heap;
+//
+//import java.lang.annotation.Inherited;
+//import java.util.Comparator;
+//import java.util.PriorityQueue;
+//
+//class Pair{
+//    int key;
+//    int value;
+//    Pair(int k, int v){
+//        this.key=k;
+//        this.value=v;
+//    }
+//    int getKey(){
+//        return key;
+//    }
+//    void setKey(int k){
+//        this.key=k;
+//    }
+//    int getValue(){
+//        return value;
+//    }
+//    void setValue(int v){
+//        this.value=v;
+//    }
+//
+//}
+//
+//
+//
+//public class KthClosestElements {
+//    static void kthClosest(int arr[], int x, int k){
+//        PriorityQueue<Pair> pq = new PriorityQueue<>(
+//                new Comparator<Pair>()
+//                {
+////                    @Override
+////                    public int compare(Pair p1, Pair p2)
+////                    {
+////                        return p2.getValue().compareTo(
+////                              p1.getValue());
+////                    }
+//                });
+//
+//        for(int i = 0; i < k; i++)
+//        {
+//            pq.offer(new Pair(arr[i],
+//                    Math.abs(arr[i] - x)));
+//        }
+//
+//        for(int i = k; i < arr.length; i++)
+//        {
+//            int diff = Math.abs(arr[i] - x);
+//
+//            if(pq.peek().getValue()>diff) {
+//                pq.poll();
+//                pq.offer(new Pair(arr[i], diff));
+//            }
+//        }
+//
+//        while(!pq.isEmpty())
+//        {
+//            System.out.print(pq.poll().getKey() + " ");
+//        }
+//
+//
+//    }
+//    public static void main(String[] args) {
+//        int arr[]={40,41,2,4,12,5,6,113,77};
+//        int num=43;
+//        int k=3;
+//       kthClosest(arr,num ,k );
+//    }
+//}
--- Heap/MergeKSortedArray.java
+++ Heap/MergeKSortedArray.java
@@ -1,6 +1,9 @@
 package Codes.Heap;
 
+import java.net.Inet4Address;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 class Triplet implements Comparable<Triplet> {
     int value, position, valIndex;
@@ -20,7 +23,48 @@ class Triplet implements Comparable<Triplet> {
 
 
 public class MergeKSortedArray {
+
+    static ArrayList<Integer> mergeArr(ArrayList<ArrayList<Integer>> arr){
+        PriorityQueue<Triplet> pq= new PriorityQueue<>();
+        ArrayList<Integer> res= new ArrayList<>();
+        for(int i=0;i<arr.size();i++)
+            pq.add(new Triplet(arr.get(i).get(0), i,0));
+        while (!pq.isEmpty()){
+            Triplet curr=pq.poll();
+            res.add(curr.value);
+            int arrPos=curr.position;
+            int valPos=curr.valIndex;
+            if(valPos+1<arr.get(arrPos).size())
+                pq.add(new Triplet(arr.get(arrPos).get(valPos+1),arrPos,valPos+1));
+
+        }
+        return res;
+
+
+
+    }
     public static void main(String[] args) {
+        ArrayList<ArrayList<Integer> > arr=new ArrayList<ArrayList<Integer>>();
+
+        ArrayList<Integer> a1 = new ArrayList<Integer>();
+        a1.add(10);
+        a1.add(20);
+        a1.add(30);
+        arr.add(a1);
+
+        ArrayList<Integer> a2 = new ArrayList<Integer>();
+        a2.add(5);
+        a2.add(15);
+        arr.add(a2);
+
+        ArrayList<Integer> a3 = new ArrayList<Integer>();
+        a3.add(1);
+        a3.add(9);
+        a3.add(11);
+        a3.add(18);
+        arr.add(a3);
+        ArrayList<Integer> res=mergeArr(arr);
+        System.out.println(res);
 
     }
 }
