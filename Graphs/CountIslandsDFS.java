d0bdacc6971231becd8b624fcc78a755a53e68f0
Aditya Singh <adityakrsingh1999@gmail.com>
2021-07-25 16:42:00 +0530

    Graphs

---
+++ Graphs/CountIslandsDFS.java
@@ -0,0 +1,45 @@
+package Codes.Graphs;
+
+import java.util.ArrayList;
+
+import static Codes.Graphs.AdjanceyList.addEdge;
+import static Codes.Graphs.BFS.addEdges;
+
+
+public class CountIslandsDFS {
+    //No source provided and the graphs may be disconected
+    static int countIsland(ArrayList<ArrayList<Integer>>adj, int v){
+        boolean visited[]= new boolean[v];
+        int count=0;
+        for(int i=0;i<v;i++)
+            if(!visited[i]) {
+                countDFS(adj, i, visited);
+                count++;
+            }
+        return count;
+    }
+    static void countDFS(ArrayList<ArrayList<Integer>>adj, int s, boolean visited[]){
+        visited[s]=true;
+        for(int vertex:adj.get(s)){
+            if(!visited[vertex]){
+                visited[vertex]=true;
+                countDFS(adj, vertex, visited);
+            }
+
+        }
+    }
+    public static void main(String[] args) {
+        int v=7;
+        ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
+        for(int i=0;i<v;i++)
+            adj.add(new ArrayList<>());
+        addEdge(adj,0,1);
+        addEdge(adj,0,2);
+        addEdge(adj,2,3);
+        addEdge(adj,1,3);
+        addEdge(adj,4,5);
+        addEdge(adj,5,6);
+        addEdge(adj,4,6);
+        System.out.println(countIsland(adj,v));
+    }
+}
---
+++ Graphs/DFS.java
@@ -0,0 +1,40 @@
+package Codes.Graphs;
+
+import java.util.ArrayList;
+
+import static Codes.Graphs.BFS.addEdges;
+
+public class DFS {
+    static  void  dfsRec(ArrayList<ArrayList<Integer>>adj, int s, boolean[]visited){
+        visited[s]=true;
+        System.out.print(s+" ");
+        for(int vertex:adj.get(s))
+            if(!visited[vertex]) {
+                visited[vertex] = true;
+                dfsRec(adj, vertex, visited);
+            }
+    }
+    static void dfs(ArrayList<ArrayList<Integer>> adj, int s, int v){
+        boolean []visited= new boolean[v];
+        dfsRec(adj, 0,visited);
+
+    }
+    public static void main(String[] args) {
+        int v=5;
+        ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
+        for(int i=0;i<v;i++)
+            adj.add(new ArrayList<>());
+        addEdges(adj, 0,1);
+        addEdges(adj, 0,2);
+        addEdges(adj, 1, 2);
+        addEdges(adj, 1, 3);
+        addEdges(adj, 2, 3);
+        addEdges(adj, 2, 4);
+        addEdges(adj, 3, 4);
+        //Traversal with the source
+        dfs(adj,0, v);
+
+    }
+
+
+}
