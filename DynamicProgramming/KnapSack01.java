543b211e4174d6f8ea717a86b34d1b915a160a06
Aditya Singh <adityakrsingh1999@gmail.com>
2021-07-31 22:35:30 +0530

    DP

--- DynamicProgramming/KnapSack01.java
+++ DynamicProgramming/KnapSack01.java
@@ -3,17 +3,14 @@ package Codes.DynamicProgramming;
 public class KnapSack01 {
     static int knapSack(int wt[], int val[], int weight){
         int n=wt.length;
-
-
         int dp[][]= new int[n + 1][weight + 1];
 
-        for(int i=0; i<=weight; i++) {
-            dp[0][i] = 0;
-        }
+        for(int i=0; i<=weight; i++)  dp[0][i] = 0;
 
-        for(int i=0; i<=n; i++) {
-            dp[i][0] = 0;
-        }
+
+        for(int i=0; i<=n; i++) dp[i][0] = 0;
+
+
         for(int i=1;i<=n;i++){
             for(int j=1;j<=weight;j++){
                 if(wt[i-1]>j)
