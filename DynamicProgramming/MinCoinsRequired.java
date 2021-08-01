b561ba65f5aad82a8b27c3bfda3e4ce41e1a891b
Aditya Singh <adityakrsingh1999@gmail.com>
2021-08-01 22:56:15 +0530

    DP

--- DynamicProgramming/MinCoinsRequired.java
+++ DynamicProgramming/MinCoinsRequired.java
@@ -5,7 +5,7 @@ public class MinCoinsRequired {
         int n=arr.length;
         int dp[][]= new int[n+1][sum+1];
 
-        for(int i=0;i<=sum;i++) dp[0][i]=Integer.MAX_VALUE;
+        for(int i=0;i<=sum;i++) dp[0][i]=Integer.MAX_VALUE-1;
         for(int i=0;i<=n;i++)   dp[i][0]=0;
 
         for(int i=1;i<=n;i++){
