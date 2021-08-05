cb5a2b3940d05fa703d8f28f30a5eea1e5452c5a
Aditya Singh <adityakrsingh1999@gmail.com>
2021-08-05 21:23:39 +0530

    Trie

---
+++ Trie/CountDistinctRowsInABinaryMatrix.java
@@ -0,0 +1,43 @@
+package Codes.Trie;
+
+class Trie{
+    Trie child[]= new Trie[2];
+    static final int M  = 4, N = 3;
+    static int mat[][] = {{1, 0, 0},
+            {1, 1, 1},
+            {1, 0, 0},
+            {0, 1, 0}};
+    static Trie root;
+    static int countDis(){
+        Trie root= new Trie();
+        int res=0;
+        for(int row=0;row<M;row++){
+            if(insert(root, row))
+                res++;
+        }
+        return res;
+    }
+    static boolean insert(Trie root, int row){
+        Trie curr= root;
+        boolean flag=false;
+        for(int i=0;i<row; i++){
+            int index=mat[row][i];
+            if(curr.child[index]==null){
+                curr.child[index]=new Trie();
+                flag=true;
+
+            }
+            curr=curr.child[index];
+        }
+        return flag;
+    }
+
+}
+
+
+public class CountDistinctRowsInABinaryMatrix {
+    public static void main(String[] args) {
+
+        System.out.println(new Trie().countDis());
+    }
+}
