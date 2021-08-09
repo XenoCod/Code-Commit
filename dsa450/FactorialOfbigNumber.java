0573dfa0f5b7499b146f3c566cb6c4e62851ea52
Aditya Singh <adityakrsingh1999@gmail.com>
2021-08-09 10:48:06 +0530

    Arrays

---
+++ dsa450/FactorialOfbigNumber.java
@@ -0,0 +1,12 @@
+package Codes.dsa450;
+
+public class FactorialOfbigNumber {
+    public static void fact(int n) {
+
+
+    }
+    public static void main(String[] args) {
+        fact(100);
+
+    }
+}
