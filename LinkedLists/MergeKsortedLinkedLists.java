6f3edc50f0a8b1470d9bf326b89562d55779708b
Aditya Singh <adityakrsingh1999@gmail.com>
2021-09-09 19:54:50 +0530

    Merge sort Linked List

---
+++ LinkedLists/MergeKsortedLinkedLists.java
@@ -0,0 +1,15 @@
+package LinkedLists;
+
+class ListNode{
+	int val;
+	ListNode next;
+	ListNode(int _val){
+		val= _val;
+	}
+}
+
+public class MergeKsortedLinkedLists {
+	public static void main(String[] args) {
+//		ListNode [] list= {{1,4,5},{1,3,4},{2,6}};
+	}
+}
--- LinkedLists/SortLinkedLists.java
+++ LinkedLists/SortLinkedLists.java
@@ -10,41 +10,68 @@ class ListNode{
 
 public class SortLinkedLists {
 	
-	//Merge a LL
-	private ListNode sort(ListNode head) {
-		 if (head == null || head.next == null) return head;
-         ListNode mid = middleNode(head);
-         ListNode left = sort(head);
-         ListNode right = sort(mid);
-         return mergeTwoLists(left, right);
-     }
-
-     public ListNode middleNode(ListNode head) {
-         ListNode slow = head, fast = head, mid;
-         while (fast.next != null && fast.next.next != null) {
-             slow = slow.next;
-             fast = fast.next.next;
-         }
-         mid = slow.next;
-         slow.next = null;
-         return mid;
-     }
-
-     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-         ListNode dummyHead = new ListNode(-1), current = dummyHead;
-         while (l1 != null && l2 != null) {
-             if (l1.val <= l2.val) {
-                 current.next = l1;
-                 l1 = l1.next;
-             } else {
-                 current.next = l2;
-                 l2 = l2.next;
-             }
-             current = current.next;
-         }
-         current.next = l1 != null ? l1 : l2;
-         return dummyHead.next;
-	}
+	
+	public ListNode sortList(ListNode head) {
+        //Step 1: Find the midddle node
+        //Step 2: Break the list
+        //Step 3: Sort the list
+        //Step 4: MErge the list
+        if(head == null || head.next == null) return head;
+        
+        //Finding middle node
+        ListNode prev= null, slow= head, fast= head;
+        while(fast != null && fast.next != null){
+            prev= slow;
+            slow= slow.next;
+            fast= fast.next.next;
+        }
+        
+        //Breaking the list into 2
+        prev.next= null;
+        
+        ListNode left= sortList(head);
+        ListNode right = sortList(slow);
+        
+        //Merge the list
+        return merge(left, right);
+        
+    }
+    
+    private ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy= new ListNode(0);
+        ListNode curr= dummy;
+        ListNode curr1= l1;//runner for l1
+        ListNode curr2= l2;//runner for l1
+        
+        while(curr1 != null || curr2 != null){
+            if(curr1 == null){
+                curr.next= curr2;
+                break;
+            }
+            
+            else if(curr2 == null){
+                curr.next= curr1;
+                break;
+            }
+            
+            else{
+                if(curr1.val <= curr2.val){
+                    curr.next= curr1;
+                    curr1= curr1.next;
+                }
+                else{
+                    curr.next= curr2;
+                    curr2= curr2.next;
+                }
+                
+                curr= curr.next;
+            }
+            
+            
+        }
+        
+        return dummy.next;
+    }
 	
 	public static void main(String[] args) {
 		ListNode head= new ListNode(4);
@@ -52,7 +79,10 @@ public class SortLinkedLists {
 		head.next.next= new ListNode(3);
 		head.next.next.next= new ListNode(1);
 		SortLinkedLists ob= new SortLinkedLists();
-		ListNode res=ob.sort(head);
-		while(res!= null) System.out.println(res.val);
+		ListNode res=ob.sortList(head);
+		while(res!= null) { 
+			System.out.print(res.val+" "); 
+			res= res.next;
+		} 
 	}
 }
