ba8e7252c4607a075602490282be993ed744634b
Aditya Singh <adityakrsingh1999@gmail.com>
2021-09-10 18:23:41 +0530

    Stack Playlist

--- Stack/StockSpanProblem.java
+++ Stack/StockSpanProblem.java
@@ -1,5 +1,30 @@
 package Stack;
 
-public class StockSpanProblem {
+import java.util.HashMap;
+import java.util.Stack;
 
+public class StockSpanProblem {
+	void stockSpan(int nums[]) {
+		 Stack<Integer> st= new Stack<>();
+			
+			
+			
+			int res[]= new int [nums.length];
+			for(int i=0; i< nums.length; i++) {
+				if(st.isEmpty())  res[i]= 1;
+				else {
+					while(!st.isEmpty() && nums[i] >= nums[st.peek()]) st.pop();
+					
+					res[i]= st.isEmpty() ? i+1: i- st.peek();
+				}
+				st.push(i);
+			}
+		
+		for(int i:res) System.out.print(i+" ");
+	}
+	public static void main(String[] args) {
+		int nums[]= {100, 80, 60, 70, 60, 75, 85};
+		StockSpanProblem obj= new StockSpanProblem();
+		obj.stockSpan(nums);
+	}
 }
