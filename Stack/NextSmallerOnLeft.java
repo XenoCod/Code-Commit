9ddefbf627f7d8ac97bd73d6a63429c0a50037b8
Aditya Singh <adityakrsingh1999@gmail.com>
2021-09-10 16:39:21 +0530

    Stack Playlist

---
+++ Stack/NextSmallerOnLeft.java
@@ -0,0 +1,32 @@
+package Stack;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class NextSmallerOnLeft {
+	void nsel(int nums[]) {
+		Stack<Integer> st= new Stack<>();
+		ArrayList<Integer> list= new ArrayList<>();
+		
+		for(int i=0; i< nums.length; i++) {
+			if(st.isEmpty()) list.add(-1);
+			else {
+				while(!st.isEmpty() && nums[i] <= st.peek()) st.pop();
+				if(st.isEmpty()) list.add(-1);
+				else list.add(st.peek());
+			}
+			
+			st.push(nums[i]);
+		}
+		
+		
+		System.out.println(list);
+	}
+	public static void main(String[] args) {
+		
+		int nums[]= {1,4,6,2,5,9};
+		NextSmallerOnLeft obj= new NextSmallerOnLeft();
+		obj.nsel(nums);
+	}
+
+}
---
+++ Stack/NextSmallerOnRight.java
@@ -0,0 +1,35 @@
+package Stack;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
+public class NextSmallerOnRight {
+	
+	void nse(int nums[]) {
+		Stack<Integer> st= new Stack<>();
+		ArrayList<Integer> list= new ArrayList<>();
+		
+		for(int i=nums.length-1; i>=0; i--) {
+			if(st.isEmpty()) list.add(-1);
+			else {
+				while(!st.isEmpty() && nums[i] <= st.peek()) st.pop();//remove the top element if its greater than
+				if(st.isEmpty()) list.add(-1);
+				else list.add(st.peek());
+			}
+			
+			st.push(nums[i]);
+		}
+		
+		Collections.reverse(list);
+		System.out.println(list);
+	}
+	
+	public static void main(String[] args) {
+		NextSmallerOnRight obj= new NextSmallerOnRight();
+//		int nums[]= {1,4,6,2,5,9};
+		int nums[]= {4, 5, 2, 10, 8};
+		
+		obj.nse(nums);
+	}
+}
