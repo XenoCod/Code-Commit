b4e3d3010f17bd67817c7a5c09c2fad8dba3c488
Aditya Singh <adityakrsingh1999@gmail.com>
2021-09-22 20:38:58 +0530

    Catalan numbers

---
+++ Tree/Catalan_Numbers.java
@@ -0,0 +1,25 @@
+package Tree;
+
+public class Catalan_Numbers {
+	static void catalan(int n) {
+		int dp[]= new int [n+1];
+		dp[0]=1;
+		dp[1]=1;
+		
+		System.out.print(dp[0] +" "+ dp[1]+" ");
+		for(int i=2; i<=n;i++) {
+			for(int j=0; j<i; j++) {
+				dp[i] += dp[j] * dp[i-j-1];
+				
+			}
+			
+			System.out.print(dp[i]+" ");
+		}
+		
+	}
+	public static void main(String[] args) {
+		//Find the nth catalan numbers
+		int n=5;
+		catalan(n);
+	}
+}
