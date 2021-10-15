86327b6a21e6e0af5937f30dd93c69ea9e22b8eb
Aditya Singh <adityakrsingh1999@gmail.com>
2021-10-15 16:49:31 +0530

    Matrix chain Multiplication

--- DynamicProgramming/MinCostTreeFromLeaf.java
+++ DynamicProgramming/MinCostTreeFromLeaf.java
@@ -1,11 +1,11 @@
 public class MinCostTreeFromLeaf{
-	static int mctFromLeaf(int nums[]){
+	int mctFromLeaf(int nums[]){
 		int n= nums.length;
 		int memo[][]= new int[n][n];
 		return solve(nums, 0, n-1, memo);
 	}
 
-	private static int solve(int nums[], int startIndex, int endIndex, int memo[][]){
+	private int solve(int nums[], int startIndex, int endIndex, int memo[][]){
 		if(startIndex >= endIndex) return 0;
 
 		if(memo[startIndex][endIndex] != 0) return memo[startIndex][endIndex];
@@ -13,24 +13,28 @@ public class MinCostTreeFromLeaf{
 		int res=Integer.MAX_VALUE;
 		for(int k=startIndex; k< endIndex; k++){
 
-			int left= solve(nums, startIndex, k, memo);
-			int right= solve(nums, k+1, endIndex, memo);
+			int rootVal= max(nums, startIndex, k) * max(nums, k+1, endIndex);
 
-			int leftMax=0, rightMax=0;
+			int non_leaf_node_from_left_subtree= solve(nums, startIndex, k, memo);
+			int non_leaf_node_from_right_subtree= solve(nums, k+1, endIndex, memo);
 
-			for(int i=startIndex; i<=k; i++) leftMax= Math.max(leftMax, nums[i]);
+			res= Math.min(res, rootVal + non_leaf_node_from_right_subtree + non_leaf_node_from_left_subtree);
 
-			for(int i=k+1; i<=endIndex; i++) rightMax= Math.max(rightMax, nums[i]);
 
-			int nonLeafNode= leftMax * rightMax;
-
-			res= Math.min(res, nonLeafNode + left + right);
 		}
 		memo[startIndex][endIndex]= res;
 		return res;
 	}
+
+	private int max(int nums[], int l, int r){
+		int res= Integer.MIN_VALUE;
+		for(int i=l; i<=r; i++)
+			res = Math.max(res, nums[i]);
+		return res;
+	}
 	public static void main(String[] args) {
 		int nums[]={6,2,4};
-		System.out.println(mctFromLeaf(nums));
+		MinCostTreeFromLeaf obj= new MinCostTreeFromLeaf();
+		System.out.println(obj.mctFromLeaf(nums));
 	}
 }
