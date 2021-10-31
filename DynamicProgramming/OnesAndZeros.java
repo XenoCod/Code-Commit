72dc8e23bc703cd5c58586108f7d78de96caec6b
Aditya Singh <adityakrsingh1999@gmail.com>
2021-10-31 22:50:03 +0530

    Ones and Zeros --DP

--- DynamicProgramming/OnesAndZeros.java
+++ DynamicProgramming/OnesAndZeros.java
@@ -1,4 +1,33 @@
 class OnesAndZeros{
+
+	  public static int findMaxForm(String[] strs, int m, int n) {
+        int dp[][]= new int[m+1][n+1];
+        for(String s: strs){
+            int count[]= getCount(s);
+            //count[0]=zeros in curr string and count[1]= ones in curr str
+            for(int zero=m; zero>= count[0]; zero--){
+                for(int one=n; one >= count[1]; one--){
+                    dp[zero][one]= Math.max(dp[zero - count[0]][one - count[1]] + 1, 
+                                           dp[zero][one]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    
+    //retuns the count of zeros and ones in curr string
+    private  static int[] getCount(String s){
+        int count[]= new int[2];
+        for(char c: s.toCharArray()){
+            if(c == '0') count[0]++;
+            else count[1]++;
+        }
+        return count;
+    }
+
+
+
+
 	public static void main(String[] args) {
 
 
@@ -14,6 +43,8 @@ class OnesAndZeros{
 // Other valid but smaller subsets include {"0001", "1"} and {"10", "1", "0"}.
 // {"111001"} is an invalid subset because it contains 4 1's, greater than the maximum of 3.
 
-		
+		String strs[]={"10","0001","111001","1","0"};
+		int m=5, n=3;
+				System.out.println(findMaxForm(strs, m, n));
 	}
 }
