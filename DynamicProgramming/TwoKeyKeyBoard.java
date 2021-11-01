8e5be6ecc665d20ad918d5c49d25b49b754dba25
Aditya Singh <adityakrsingh1999@gmail.com>
2021-11-01 20:14:25 +0530

    2 keys keyboard ---DP

---
+++ DynamicProgramming/TwoKeyKeyBoard.java
@@ -0,0 +1,13 @@
+class TwoKeyKeyBoard{
+	public static void main(String[] args) {
+
+// Input: n = 3
+// Output: 3
+// Explanation: Intitally, we have one character 'A'.
+// In step 1, we use Copy All operation.
+// In step 2, we use Paste operation to get 'AA'.
+// In step 3, we use Paste operation to get 'AAA'.
+
+		
+	}
+}
