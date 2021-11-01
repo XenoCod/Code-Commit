52b6f3724515eb057cc0641fb2889a77b2026c62
Aditya Singh <adityakrsingh1999@gmail.com>
2021-11-01 22:25:28 +0530

    2 keys keyboard ---DP

--- DynamicProgramming/TwoKeyKeyBoard.java
+++ DynamicProgramming/TwoKeyKeyBoard.java
@@ -1,4 +1,32 @@
 class TwoKeyKeyBoard{
+
+	static private int minSteps(int n){
+		 if(n == 1) return 0;
+        if(n == 2) return 2;
+        
+        
+        int dp[]= new int [n + 1];
+        dp[1] = 0;
+        dp[2]= 2;
+        
+        for(int i= 3; i<=n; i++){
+            dp[i] = i;//Since to get ans one of the option is to copy it n times
+            
+            int j= i / 2;
+            //SInce the max multiple of n should start from n /2 
+            while(j >= 1){
+                if(i % j == 0){
+                    dp[i]= Math.min(dp[i], dp[j] + i / j);
+                    break;
+                }
+                j--;
+            }
+        }
+        
+     return dp[n];  
+	}
+
+
 	public static void main(String[] args) {
 
 // Input: n = 3
@@ -7,7 +35,8 @@ class TwoKeyKeyBoard{
 // In step 1, we use Copy All operation.
 // In step 2, we use Paste operation to get 'AA'.
 // In step 3, we use Paste operation to get 'AAA'.
+			int n=3;
+					System.out.println(minSteps(n));
 
-		
 	}
 }
