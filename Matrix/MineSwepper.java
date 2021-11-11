12706407d5d7558510fbd8550b1c4d2d08935ec9
Aditya Singh <adityakrsingh1999@gmail.com>
2021-11-11 21:43:38 +0530

    MineSwepper --DFS

---
+++ Matrix/MineSwepper.java
@@ -0,0 +1,15 @@
+class MineSwepper{
+
+	private static char [][] updateBoard(char board[][], int click[]){
+		
+	}
+
+
+
+	public static void main(String[] args) {
+		char board[][]={{'E','E','E','E','E'}, {'E', 'E', 'M', 'E','E'}, {'E','E','E','E','E'}, {'E','E','E','E','E'}};
+		int click[]={3, 0};
+
+		updateBoard(board, click);
+	}
+}
