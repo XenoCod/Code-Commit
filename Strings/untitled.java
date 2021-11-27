350dc31d4f08decca332f893256467671adb7302
Adi's Mac <adityakrsingh1999@gmail.com>
2021-11-27 23:11:18 +0530

    Min Buckets to collect max rainwater LC Contest

--- Strings/untitled.java
+++ Strings/untitled.java
@@ -1,22 +1,33 @@
-//Count the vowels of all substring of a string
-// Input: word = "aba"
-// Output: 6
-// Explanation: 
-// All possible substrings are: "a", "ab", "aba", "b", "ba", and "a".
-// - "b" has 0 vowels in it
-// - "a", "ab", "ba", and "a" have 1 vowel each
-// - "aba" has 2 vowels in it
-// Hence, the total sum of vowels = 0 + 1 + 1 + 1 + 1 + 2 = 6.
+/*
+Input: street = "H..H"
+Output: 2
+Explanation:
+We can put buckets at index 1 and index 2.
+"H..H" -> "HBBH" ('B' denotes where a bucket is placed).
+The house at index 0 has a bucket to its right, and the house at index 3 has a bucket to its left.
+Thus, for every house, there is at least one bucket collecting rainwater from it.
+*/
 
-public class VowelsOfStrings{
+class MinBucketsToCollectRainwater{
 	public static void main(String[] args) {
-		 long res=0, prev= 0;
-        for(int i=0; i< word.length(); i++){
-            char c= word.charAt(i);
-            if(c == 'a' || c == 'e' || c =='i' || c == 'o' || c=='u')
-                prev += (i + 1);
-            res += prev;
+		int count=0;
+        int n= s.length();
+        
+        for(int i=0; i< n; i++){
+            if(s.charAt(i) == 'H'){
+                if(i + 1 < n && s.charAt(i+1) == '.'){
+                    count++;
+                    i += 2;
+                    continue;
+                }
+                
+                if(i - 1 >= 0 && s.charAt(i-1) == '.'){
+                    count++;
+                    continue;
+                }
+                return -1;
+            }
         }
-        return res;
+        return count;
 	}
-} 
+}
